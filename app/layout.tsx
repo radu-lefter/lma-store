cc7e05a684049ecf07e6557b2330a93e6ec61433
raduubraduu <lefter_radu@yahoo.com>
2023-07-06 13:38:16 +0100

    created footer and main navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,27 @@
-import './globals.css'
-import { Urbanist } from 'next/font/google'
+import "./globals.css";
+import { Urbanist } from "next/font/google";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 
-const font = Urbanist({ subsets: ['latin'] })
+const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Store',
-  description: 'Store',
-}
+  title: "Store",
+  description: "Store",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
